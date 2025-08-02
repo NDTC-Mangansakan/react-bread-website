08ee3ff3dd2902c77a24717d7169711be91e974f
Shaq <mangansakansansudin@gmail.com>
2025-08-03 07:37:40 +0800

    modified navbar nav items active link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,49 @@
 import { RiCloseFill, RiMenu2Fill } from '@remixicon/react'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+
+
+const navItems = [
+    { name: 'Home', href: '#home' },
+    { name: 'New', href: '#new' },
+    { name: 'About Us', href: '#about' },
+    { name: 'Favourites', href: '#favourites' },
+    { name: 'Location', href: '#location' }
+]
 
 const Navbar = () => {
 
     const [showNav, setShowNav] = useState(false)
+    const [activeSection, setActiveSection] = useState('')
 
     const handleShowNav = () => {
         setShowNav(prev => !prev)
     }
+
+    useEffect(() => {
+        const observerOptions = {
+            root: null,
+            rootMargin: '-50% 0px -50% 0px', // triggers when section is around middle
+            threshold: 0,
+        };
+
+        const observerCallback = (entries) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    setActiveSection(`#${entry.target.id}`);
+                }
+            });
+        };
+
+        const observer = new IntersectionObserver(observerCallback, observerOptions);
+
+        // Observe all sections
+        navItems.forEach(item => {
+            const section = document.querySelector(item.href);
+            if (section) observer.observe(section);
+        });
+
+        return () => observer.disconnect();
+    }, [])
 
     return (
         <nav className='h-[10vh] fixed top-0 right-0 left-0 flex items-center z-99 bg-transparent backdrop-blur-xs'>
@@ -20,11 +56,17 @@ const Navbar = () => {
 
                 {/* Nav links for screens below lg */}
                 <div className={`bg-gray-950 fixed top-0 ${showNav ? 'right-0' : '-right-[270px]'} w-[250px] h-screen flex flex-col gap-10 pt-20 px-5 transition-all duration-300 lg:hidden shadow-[0_5px_25px_rgba(255,255,255,0.742)]`}>
-                    <a href="#home" className='inline-block text-3xl' onClick={handleShowNav}>Home</a>
-                    <a href="#new" className='inline-block text-3xl' onClick={handleShowNav}>New</a>
-                    <a href="#about" className='inline-block text-3xl' onClick={handleShowNav}>About us</a>
-                    <a href="#favourites" className='inline-block text-3xl' onClick={handleShowNav}>Favourites</a>
-                    <a href="#location" className='inline-block text-3xl' onClick={handleShowNav}>Location</a>
+                    {navItems.map(item => (
+                        <a
+                            href={item.href}
+                            key={item.href}
+                            onClick={handleShowNav}
+                            className={`inline-block text-3xl ${activeSection === item.href ? 'text-primary font-bold' : ''
+                                }`}
+                        >
+                            {item.name}
+                        </a>
+                    ))}
                     <span className="absolute top-5 right-5 cursor-pointer" onClick={handleShowNav}>
                         <RiCloseFill size={40} />
                     </span>
@@ -32,11 +74,15 @@ const Navbar = () => {
 
                 {/* Nav links for lg screens and above */}
                 <div className="hidden lg:flex items-center gap-10">
-                    <a href="#home" className='' onClick={handleShowNav}>Home</a>
-                    <a href="#new" className='' onClick={handleShowNav}>New</a>
-                    <a href="#about" className='' onClick={handleShowNav}>About us</a>
-                    <a href="#favourites" className='' onClick={handleShowNav}>Favourites</a>
-                    <a href="#location" className='' onClick={handleShowNav}>Location</a>
+                    {navItems.map(item => (
+                        <a href={item.href}
+                            key={item.href}
+                            className={`${activeSection === item.href ? 'text-primary font-bold' : ''
+                                }`}
+                            onClick={handleShowNav}>
+                            {item.name}
+                        </a>
+                    ))}
                 </div>
             </div>
         </nav>
